b9cf14d7e1e074e2130ec8a97aa93201ecbbb35c
Shoya <t120575st@gmail.com>
2025-10-25 22:15:46 +0900

    PWAスクロール無効化

--- src/components/RecipeList/CategoryItem.tsx
+++ src/components/RecipeList/CategoryItem.tsx
@@ -11,6 +11,12 @@ import {
 } from "@/components/ui/collapsible";
 import { RecipeItem } from "./RecipeItem";
 import { Recipe } from "../../modules/recipes/recipe.entity";
+import meatIcon from "../../assets/meat.webp";
+import fishIcon from "../../assets/fish.webp";
+import donIcon from "../../assets/don.webp";
+import menIcon from "../../assets/men.webp";
+import kozareIcon from "../../assets/kozara.webp";
+import otherIcon from "../../assets/other.webp";
 
 interface CategoryItemProps {
   category: string;
@@ -38,8 +44,50 @@ export const CategoryItem = ({
         onClick={handleOpen}
         className="pl-2 py-1.5 px-1 outline-none w-full"
       >
-        <div role="button" className="flex items-center">
+        <div role="button" className="flex gap-1 items-center">
           {isOpen ? <ChevronDown /> : <ChevronRight />}
+          {category === "肉料理" && (
+            <img
+              src={meatIcon}
+              alt="meat icon"
+              className="w-7 h-7 lg:w-7 lg:h-7"
+            />
+          )}
+          {category === "魚料理" && (
+            <img
+              src={fishIcon}
+              alt="fish icon"
+              className="w-7 h-7 lg:w-7 lg:h-7"
+            />
+          )}
+          {category === "丼・ルー料理" && (
+            <img
+              src={donIcon}
+              alt="dish icon"
+              className="w-7 h-7 lg:w-7 lg:h-7"
+            />
+          )}
+          {category === "麺料理" && (
+            <img
+              src={menIcon}
+              alt="noodle icon"
+              className="w-7 h-7 lg:w-7 lg:h-7"
+            />
+          )}
+          {category === "小物" && (
+            <img
+              src={kozareIcon}
+              alt="kozare icon"
+              className="w-7 h-7 lg:w-7 lg:h-7"
+            />
+          )}
+          {category === "その他" && (
+            <img
+              src={otherIcon}
+              alt="other icon"
+              className="w-7 h-7 lg:w-7 lg:h-7"
+            />
+          )}
           <span className="font-bold text-gray-600 text-lg">{category}</span>
           <span className="ml-3 font-normal">{recipes.length} 件</span>
         </div>
--- src/components/RecipeList/RecipeItem.tsx
+++ src/components/RecipeList/RecipeItem.tsx
@@ -1,29 +1,37 @@
 //サイドバーに表示するレシピ本体
 //タイトルを表示しており、クリックするとレシピの詳細画面に遷移する
 
-import { Recipe } from '../../modules/recipes/recipe.entity';
-import {useNavigate} from 'react-router-dom';
-import { Trash2 } from 'lucide-react';
+import { Recipe } from "../../modules/recipes/recipe.entity";
+import { useNavigate } from "react-router-dom";
+import { Trash2 } from "lucide-react";
 
 interface RecippeItemProps {
-    recipe: Recipe;
-    ondeleteRecipe: (id: number) => Promise<void>;
-    setOpen: (open: boolean) => void;
+  recipe: Recipe;
+  ondeleteRecipe: (id: number) => Promise<void>;
+  setOpen: (open: boolean) => void;
 }
 
-export const RecipeItem = ({recipe,ondeleteRecipe,setOpen}:RecippeItemProps) => {
+export const RecipeItem = ({
+  recipe,
+  ondeleteRecipe,
+  setOpen,
+}: RecippeItemProps) => {
+  const navigate = useNavigate();
+  const moveToDetail = () => {
+    setOpen(false);
+    navigate(`/recipes/${recipe.id}`);
+  };
 
-    const navigate = useNavigate();
-    const moveToDetail = () => {
-        setOpen(false)
-        navigate(`/recipes/${recipe.id}`)
-    }
-
-    return(
-        <div className="flex items-center cursor-pointer hover:bg-gray-200 pl-8 py-1.5 text-lg text-gray-700">
-            {/* truncate:文字が長すぎる場合に省略する。（その場合は…と表示される） */}
-            <span className="truncate flex-1" onClick={moveToDetail}>{recipe.title}</span>
-            <Trash2 className="w-5 h-5 text-gray-500 ml-auto mr-2" onClick={() => ondeleteRecipe(recipe.id)}/>
-        </div>
-    )
-}
+  return (
+    <div className="flex items-center cursor-pointer hover:bg-gray-200 pl-10 py-1.5 text-lg text-gray-700">
+      {/* truncate:文字が長すぎる場合に省略する。（その場合は…と表示される） */}
+      <span className="truncate flex-1" onClick={moveToDetail}>
+        {recipe.title}
+      </span>
+      <Trash2
+        className="w-5 h-5 text-gray-500 ml-auto mr-2"
+        onClick={() => ondeleteRecipe(recipe.id)}
+      />
+    </div>
+  );
+};
